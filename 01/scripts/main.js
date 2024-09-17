c6b7938231770f8da1362101573f1e17e07be471
arnmary <marinaarnaut482@gmail.com>
2024-09-17 11:29:35 +0300

    HW#1 створення змінних

---
+++ 01/scripts/main.js
@@ -0,0 +1,49 @@
+
+// alert('Hello world! from separated file')
+
+// let firstValue = 1;
+// let secondValue = 2;
+// let summResult =firstValue+secondValue;
+
+// let mynumber = 23;
+// let floatnumber = 3.14;
+// let InfinityValue = Infinity;
+// let NaNnumber = NaN;
+
+//  console. log (mynumber);
+//  console.log(floatnumber);
+//  console.log(InfinityValue);
+//  console.log(NaNnumber);
+
+//  HW#01
+ let userNickname ='Maryna';
+ let userScore = 10;
+ let userAge =39;
+
+ console.log(userAge);
+
+ let firstValue = 5;
+ let firstAgeValue = userAge+firstValue;
+
+ console.log(firstAgeValue);
+
+ let isStudent = 'true';
+
+ console.log(isStudent);
+
+ isStudent = 'false';
+
+ console.log(isStudent);
+
+ let item = 10;
+
+ console.log(item);
+ 
+  item ='ten';
+ console.log(item);
+
+ item =10;
+ console.log(item);
+
+item ='true';
+console.log(item);
